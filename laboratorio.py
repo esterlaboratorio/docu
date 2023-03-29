cdcfd63fa9fd6f037028702a74fee19573c89c72
Ester Garcia <128774979+esterlaboratorio@users.noreply.github.com>
2023-03-29 11:25:00 -0600

    Add files via upload

---
+++ laboratorio.py
@@ -0,0 +1,15 @@
+#Pedimos al usuario que ingrese los dos valores numéricos
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Realizamos las operaciones aritméticas básicas
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+# Mostramos los resultados al usuario
+print(f"La suma de {num1} y {num2} es: {suma}")
+print(f"La resta de {num1} y {num2} es: {resta}")
+print(f"La multiplicación de {num1} y {num2} es: {multiplicacion}")
+print(f"La división de {num1} y {num2} es: {division}")
